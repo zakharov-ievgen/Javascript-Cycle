7c47f74c909635264a1547181710650c2b2cf818
zakharov-ievgen <zakharov.ievgen.ukr@gmail.com>
2022-04-11 10:56:55 +0300

    Avengers - Final

--- JavascriptCycle.js
+++ JavascriptCycle.js
@@ -1,6 +1,6 @@
 let i = 1;
 let b;
-for (i; (b = prompt("choose your action")); i++) {
+for (i; (b = prompt("Choose your action - break or continue")); i++) {
     if (b === "continue") continue;
     else if (b === "break") break;
 }
